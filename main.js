758d82cca86b6925bcef7d8a4fbe36d62be6e091
$randRange_ <34937272+LosEvons@users.noreply.github.com>
2022-10-25 16:07:07 +0300

    some random additions

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let wood_harvesters = 0;
+let stone_harvesters = 0;
+
 function harvestWood()
 {
     var value = parseInt(document.getElementById('wood-count').value, 10);
@@ -13,3 +16,31 @@ function harvestStone()
     value++;
     document.getElementById('stone-count').value = value;
 }
+
+function addWoodHarvester()
+{
+    wood_harvesters++;
+}
+
+function processHarvesters()
+{
+    var wood_value = parseInt(document.getElementById('wood-count').value, 10);
+    wood_value = isNaN(value) ? 0 : wood_value;
+    wood_value += wood_harvesters;
+
+    var stone_value = parseInt(document.getElementById('stone-count').value, 10);
+    stone_value = isNaN(stone_value) ? 0 : stone_value;
+    stone_value += stone_harvesters;
+}
+
+function addStoneHarvester()
+{
+    stone_harvesters++;
+}
+
+(function main()
+{
+    console.log("test");
+    processHarvesters();
+    setTimeout(main, 1000);
+})();
