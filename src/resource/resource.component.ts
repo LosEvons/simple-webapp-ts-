de2918c1d3c93a2bb54dabd9f812bfd289873a52
$randRange_ <34937272+LosEvons@users.noreply.github.com>
2022-10-31 12:58:27 +0200

    Worked on the progress bar, which now starts. Unfortunately it doesn't stop. Kind of a bummer.

--- src/resource/resource.component.ts
+++ src/resource/resource.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Resource } from 'src/DTO/resource';
 import { ResourceService } from 'src/services/resource.service';
@@ -11,7 +12,7 @@ export class ResourceComponent implements OnInit {
   @Input()
   resource: Resource | undefined;
 
-
+  counter = 0;
   title = "title"
 
   constructor(private resourceService: ResourceService) { }
@@ -32,7 +33,23 @@ export class ResourceComponent implements OnInit {
     if (progressive_bar != null) {
       progressive_bar.textContent = "Progressing epicly";
     }
+    this.updateProgressBar();
     setTimeout(() => this.buyAutoHarvester(), 10000);
+  }
+
+  updateProgressBar() {
+    if (this.counter < 100){
+      this.counter += 10;
+      let progressive_bar_bar = document.getElementById("progressive-bar-bar");
+      if (progressive_bar_bar != null) {
+        let progressive_bar_bar_width = document.getElementById("progressive-bar-bar")?.style.width.valueOf();
+        progressive_bar_bar_width?.replace('%', ' ');
+        let new_progress = progressive_bar_bar_width as unknown as number;
+        new_progress = new_progress + 1;
+        progressive_bar_bar.style.width = this.counter + "%";
+      }
+      var active_progress_bar = setTimeout(()=> this.updateProgressBar(), 1000);
+    }
   }
 
   buyAutoHarvester() {
@@ -44,7 +61,11 @@ export class ResourceComponent implements OnInit {
     if (progressive_bar != null) {
       progressive_bar.textContent = "Idle"
     }
-
+    let progressive_bar_bar = document.getElementById("progressive-bar-bar");
+    if (progressive_bar_bar != null) {
+      progressive_bar_bar.style.width = "0" + "%";
+    }
+    this.counter = 0;
   }
 
   improveAutoHarvester() {
